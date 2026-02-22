c1b4f0082c2462db476610ed8435d82babfea4ed
Manish kumar <manishnkb7@gmail.com>
2026-02-23 02:03:22 +0530

    adding todoItem component which takes input from user about todos

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function ToDoItem({ todo, deleteTodo, toggleComplete, editTodo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newText, setNewText] = useState(todo.text);
+
+  const handleEdit = () => {
+    editTodo(todo.id, newText);
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="todo-item">
+      {isEditing ? (
+        <>
+          <input
+            value={newText}
+            onChange={(e) => setNewText(e.target.value)}
+          />
+          <button onClick={handleEdit}>Save</button>
+        </>
+      ) : (
+        <>
+          <span
+            onClick={() => toggleComplete(todo.id)}
+            className={todo.completed ? "completed" : ""}
+          >
+            {todo.text}
+          </span>
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default ToDoItem;
